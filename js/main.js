4eaa562afb0252ad21a4247986eba23b2caa6974
Mitensu <Marcin.kuczma@gmail.com>
2023-01-19 16:20:29 +0100

    navbar-ready

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Open menu */
+document.getElementsByClassName("hamburger")[0].addEventListener('click', function() {
+    document.getElementsByClassName('open-menu-container')[0].classList.toggle("open")
+})
+/* Close menu */
+document.getElementsByClassName("mobile-close")[0].addEventListener('click', function() {
+    document.getElementsByClassName('open-menu-container')[0].classList.toggle("open")
+})
